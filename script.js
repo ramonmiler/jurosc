cff0b113925ba1fa6d418d27809d44ab40f79e01
ramonmiler <148217426+ramonmiler@users.noreply.github.com>
2025-09-08 14:45:27 +0000

    salvar

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function (){
+    // Utilitário para formatar moeda (R$)
+    function moedaBR(valor) {
+        return new Intl.NumberFormat('pt-BR', {style: 'currency', currency:
+'BRL' }).format(valor);
+        }
 
+    // normaliza entrada (troca vírgula por ponto e converte para número)
+    function toNumber(val) {
+        if (typeof val === 'number') return val;
+        if (!val && val !== 0) return NaN;
+        return parseFloat(String(val).trim().replace(',','.'));
+    }
+    
+    }
+)
